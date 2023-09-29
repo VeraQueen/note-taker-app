307571002ec8f7314a25c672a25bbbdd84893962
Veronika Javor <veronika.javor@gmail.com>
2023-09-29 17:10:37 +0200

    added basic routing(main nav)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyPlaylistsComponent } from './my-playlists/my-playlists.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'playlists',
+    component: MyPlaylistsComponent,
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { PlaylistsComponent } from './playlists/playlists.component';
-import { PlaylistComponent } from './playlist/playlist.component';
-import { NotesComponent } from './notes/notes.component';
+import { PlaylistsComponent } from './my-playlists/playlists/playlists.component';
+import { PlaylistComponent } from './my-playlists/playlist/playlist.component';
+import { NotesComponent } from './my-playlists/notes/notes.component';
+import { MyPlaylistsComponent } from './my-playlists/my-playlists.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, PlaylistsComponent, PlaylistComponent, NotesComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PlaylistsComponent,
+    PlaylistComponent,
+    NotesComponent,
+    MyPlaylistsComponent,
+    SearchComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
