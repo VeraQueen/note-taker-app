a078dd748eec4e1de5e37c910f75d486527b4f65
Veronika Javor <veronika.javor@gmail.com>
2023-09-30 21:00:53 +0200

    started working on the search input; added reactive setup

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  searchForm: FormGroup;
 
+  ngOnInit() {
+    this.searchForm = new FormGroup({
+      searchInput: new FormControl(null, Validators.required),
+    });
+  }
+
+  onSearch() {
+    console.log(this.searchForm.value);
+  }
 }
