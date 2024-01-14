03d2e5ff2c3a4216a75280f0d97ea49f0656d4e0
Veronika Javor <veronika.javor@gmail.com>
2024-01-14 19:05:52 +0100

    started implementing youtube player api which will play the fetched video

--- src/app/my-playlists/notes/notes.component.ts
+++ src/app/my-playlists/notes/notes.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.css']
+  styleUrls: ['./notes.component.css'],
 })
 export class NotesComponent {
-
+  // onYouTubeIframeAPIReady() {
+  //   const player = new YT.Player('player', {
+  //     width: 1280,
+  //     height: 720,
+  //     videoId: 'M7lc1UVf-VE',
+  //     events: {
+  //       // onReady: this.onPlayerReady(),
+  //       // onStateChange: onPlayerStateChange,
+  //       // onError: onPlayerError,
+  //     },
+  //   });
+  // }
+  // onPlayerReady(event) {
+  //   event.target.setVolume(20);
+  //   event.target.playVideo();
+  // }
 }
