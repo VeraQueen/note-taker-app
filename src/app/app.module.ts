e8c96ae4e352c75fa2b7455200f77760bf39a3c0
Veronika Javor <veronika.javor@gmail.com>
2024-01-17 10:44:12 +0100

    implemented the functionality of youtube iframe player; added the form for notes and added math responsible for playling the video by clicking on a timestamp in the note

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -36,6 +36,7 @@ import { CssLoaderComponent } from './shared/css-loader/css-loader.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     NgIconsModule.withIcons({
--- src/app/my-playlists/notes/notes.component.ts
+++ src/app/my-playlists/notes/notes.component.ts
@@ -1,25 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.css'],
 })
-export class NotesComponent {
-  // onYouTubeIframeAPIReady() {
-  //   const player = new YT.Player('player', {
-  //     width: 1280,
-  //     height: 720,
-  //     videoId: 'M7lc1UVf-VE',
-  //     events: {
-  //       // onReady: this.onPlayerReady(),
-  //       // onStateChange: onPlayerStateChange,
-  //       // onError: onPlayerError,
-  //     },
-  //   });
-  // }
-  // onPlayerReady(event) {
-  //   event.target.setVolume(20);
-  //   event.target.playVideo();
-  // }
+export class NotesComponent implements OnInit {
+  YT: any;
+  video: any;
+  videoPlayer: any;
+  reframed: boolean = false;
+  timeStamp: string = '0:00';
+  showForm: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.init();
+  }
+
+  init() {
+    let tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    let firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    window['onYouTubeIframeAPIReady'] = () => this.startVideo();
+  }
+
+  startVideo() {
+    this.videoPlayer = new window['YT'].Player('videoPlayer', {
+      videoId: 'M7lc1UVf-VE',
+      playerVars: {
+        playsinline: 1,
+        rel: 0,
+      },
+      events: {
+        onStateChange: this.onPlayerStateChange.bind(this),
+        onError: this.onPlayerError.bind(this),
+        onReady: this.onPlayerReady.bind(this),
+      },
+    });
+  }
+
+  onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  onPlayerStateChange(event) {
+    switch (event.data) {
+      case window['YT'].PlayerState.PAUSED:
+        console.log('paused');
+        this.calculateTimestamp();
+        break;
+    }
+  }
+
+  onPlayerError(event) {
+    console.log('error', event);
+  }
+
+  onTakeNote() {
+    this.calculateTimestamp();
+    this.videoPlayer.pauseVideo();
+    this.showForm = true;
+  }
+
+  onSaveNote(noteForm: NgForm) {
+    console.log(noteForm);
+    const timeStampSeconds = Math.floor(this.videoPlayer.getCurrentTime());
+    const note = noteForm.value.note;
+    console.log(timeStampSeconds, note);
+    noteForm.reset();
+  }
+
+  onCancelNote(noteForm: NgForm) {
+    noteForm.reset();
+    this.showForm = false;
+    this.videoPlayer.playVideo();
+  }
+
+  private calculateTimestamp() {
+    const minutes: number = +Math.floor(this.videoPlayer.getCurrentTime() / 60);
+    const seconds: number = +Math.floor(this.videoPlayer.getCurrentTime() % 60);
+    this.timeStamp = minutes + ':' + seconds;
+    console.log(this.timeStamp);
+  }
 }
