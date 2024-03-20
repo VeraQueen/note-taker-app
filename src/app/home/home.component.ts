4166b37b40e8a5b725e453a3a4729cbf7604f51a
Veronika Javor <veronika.javor@gmail.com>
2024-03-20 22:46:48 +0100

    added router module to imports to home.component.ts and added routerLink directive to connect the button in HomeComponent to AuthComponent; changed the styling of the button (color)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgIcon],
+  imports: [NgIcon, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
