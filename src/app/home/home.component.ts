f747db3fc2a4ee43c8bb5b23d4df33bf7a2d8a9c
Veronika Javor <veronika.javor@gmail.com>
2024-03-20 10:48:06 +0100

    added icons to home.component.html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
