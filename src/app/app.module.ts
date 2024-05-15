2a9addda053eb4331c4ff36410c3e13f1e404adf
Veronika Javor <veronika.javor@gmail.com>
2024-05-15 10:58:31 +0200

    moved the firebase specific actions (imports) to firebase.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,40 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { AppRoutingModule } from './app-routing.module';
+import { IconsModule } from './icons.module';
+import { DialogsModule } from './shared/dialogs/dialogs.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './auth/auth.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
 import { PlaylistsComponent } from './my-playlists/playlists/playlists.component';
 import { PlaylistComponent } from './my-playlists/playlist/playlist.component';
 import { PlayerComponent } from './my-playlists/player/player.component';
 import { SearchComponent } from './search/search.component';
 import { NotesComponent } from './my-playlists/player/notes/notes.component';
+
 import { ScrollTopComponent } from './shared/scroll-top/scroll-top.component';
 import { CssLoaderComponent } from './shared/css-loader/css-loader.component';
 import { ErrorComponent } from './shared/error/error.component';
 import { SuccessMessageComponent } from './shared/success-message/success-message.component';
-import { AuthComponent } from './auth/auth.component';
 import { BackBtnComponent } from './shared/back-btn/back-btn.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { AddedPlaylistDirective } from './shared/directives/added-playlist.directive';
 import { RemoveWatchedVideoDirective } from './shared/directives/removeWatchedVideo.directive';
-
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getStorage, provideStorage } from '@angular/fire/storage';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { environment } from '../environments/environment';
-
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
 import { ApostrophePipe } from './shared/pipes/apostophe.pipe';
 import { AmpersandPipe } from './shared/pipes/ampersand.pipe';
-
-import { IconsModule } from './icons.module';
-import { DialogsModule } from './shared/dialogs/dialogs.module';
+import { FirebaseModule } from './firebase.module';
 
 @NgModule({
   declarations: [
@@ -51,6 +47,7 @@ import { DialogsModule } from './shared/dialogs/dialogs.module';
   imports: [
     DialogsModule,
     IconsModule,
+    FirebaseModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
@@ -68,10 +65,6 @@ import { DialogsModule } from './shared/dialogs/dialogs.module';
     AuthComponent,
     FooterComponent,
     HomeComponent,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth()),
-    provideStorage(() => getStorage()),
   ],
   providers: [provideAnimationsAsync('noop')],
   bootstrap: [AppComponent],
---
+++ src/app/firebase.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideStorage(() => getStorage()),
+  ],
+  exports: [],
+})
+export class FirebaseModule {}
