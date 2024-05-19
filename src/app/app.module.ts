5cb894cb721cd8247d283dc1fd81cb8a74f40090
Veronika Javor <veronika.javor@gmail.com>
2024-05-19 13:39:31 +0200

    separated everything playlist/notes/player related into a new module playlists.module.ts (adjusted the imports so far)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
@@ -8,48 +8,25 @@ import { FirebaseModule } from './firebase.module';
 import { AppRoutingModule } from './app-routing.module';
 import { IconsModule } from './icons.module';
 import { DialogsModule } from './shared/dialogs/dialogs.module';
-import { DirectivesModule } from './shared/directives/directives.module';
-import { PipesModule } from './shared/pipes/pipes.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
-import { PlaylistsComponent } from './my-playlists/playlists/playlists.component';
-import { PlaylistComponent } from './my-playlists/playlist/playlist.component';
-import { PlayerComponent } from './my-playlists/player/player.component';
-import { SearchComponent } from './search/search.component';
-import { NotesComponent } from './my-playlists/player/notes/notes.component';
-
-import { ScrollTopComponent } from './shared/scroll-top/scroll-top.component';
 import { ErrorComponent } from './shared/error/error.component';
-import { BackBtnComponent } from './shared/back-btn/back-btn.component';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { PlaylistsModule } from './my-playlists/playlists.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    PlaylistsComponent,
-    PlaylistComponent,
-    PlayerComponent,
-    SearchComponent,
-    NotesComponent,
-    ScrollTopComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
-    DirectivesModule,
-    PipesModule,
+    PlaylistsModule,
     DialogsModule,
     IconsModule,
     FirebaseModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
-    InfiniteScrollModule,
-    BackBtnComponent,
     ErrorComponent,
     FooterComponent,
   ],
---
+++ src/app/my-playlists/playlists.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { SearchComponent } from '../search/search.component';
+import { NotesComponent } from './player/notes/notes.component';
+import { PlayerComponent } from './player/player.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { ScrollTopComponent } from '../shared/scroll-top/scroll-top.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { ErrorComponent } from '../shared/error/error.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BackBtnComponent } from '../shared/back-btn/back-btn.component';
+import { CommonModule } from '@angular/common';
+import { PipesModule } from '../shared/pipes/pipes.module';
+import { IconsModule } from '../icons.module';
+import { DirectivesModule } from '../shared/directives/directives.module';
+import { DialogsModule } from '../shared/dialogs/dialogs.module';
+
+@NgModule({
+  declarations: [
+    PlaylistsComponent,
+    PlaylistComponent,
+    PlayerComponent,
+    SearchComponent,
+    NotesComponent,
+    ScrollTopComponent,
+  ],
+  imports: [
+    InfiniteScrollModule,
+    ErrorComponent,
+    ReactiveFormsModule,
+    BackBtnComponent,
+    CommonModule,
+    IconsModule,
+    PipesModule,
+    DirectivesModule,
+    DialogsModule,
+  ],
+  exports: [
+    PlaylistsComponent,
+    PlaylistComponent,
+    PlayerComponent,
+    SearchComponent,
+    NotesComponent,
+  ],
+})
+export class PlaylistsModule {}
