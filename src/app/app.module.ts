e11309d7119249e1755876730fb8709f0a2c601a
Veronika Javor <veronika.javor@gmail.com>
2024-05-19 13:50:05 +0200

    removed IconsModule and ErrorComponent from AppModule since both are  imported for individual components and in the PlaylistsModule and ErrorComponent is standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,28 +6,23 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 import { FirebaseModule } from './firebase.module';
 import { AppRoutingModule } from './app-routing.module';
-import { IconsModule } from './icons.module';
 import { DialogsModule } from './shared/dialogs/dialogs.module';
+import { PlaylistsModule } from './my-playlists/playlists.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-import { ErrorComponent } from './shared/error/error.component';
-import { PlaylistsModule } from './my-playlists/playlists.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
     PlaylistsModule,
     DialogsModule,
-    IconsModule,
     FirebaseModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    ErrorComponent,
     FooterComponent,
   ],
   providers: [provideAnimationsAsync('noop')],
